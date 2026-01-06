e9014eb421e3f5a23509eb63b8f4b9c11656c5c4
fathur-pr <132331379+fathur-pr@users.noreply.github.com>
2026-01-07 02:18:15 +0700

    Update page title from BengkelPro to Alpino Jakarta

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
-    <title>Member Area - BengkelPro</title>
+    <title>Member Area - Alpino Jakarta</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
